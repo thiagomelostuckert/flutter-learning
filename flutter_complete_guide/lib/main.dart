e3586d357ec4f979fac216ba6bf7ec90fe19024b
Thiago <thiago.melo.stuckert@gmail.com>
2022-01-26 22:08:00 -0300

    add exemplo de perguntas em sequencia

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,58 +1,90 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
 /* 
 void main(){
  runApp(myApp());
 }
  */
-void main() => runApp(myApp());
+void main() => runApp(MyApp());
 
-class myApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen!');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText":
+          'Se você tivesse oportunidade iria para qual desses países?',
+      "answers": [
+        "Estados Unidos",
+        "Canadá",
+        "Australia",
+        "Irlanda",
+        "Portugal"
+      ],
+    },
+    {
+      "questionText": 'Quais idiomas você já fala?',
+      "answers": ["Inglês", "Francês", "Espanhol", "Italiano", "Alemão"],
+    },
+    {
+      "questionText": 'Quais países você já visitou de férias?',
+      "answers": [
+        "Estados Unidos",
+        "Canadá",
+        "Australia",
+        "Irlanda",
+        "Portugal"
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      this._questionIndex += 1;
+    });
+
+    if (_questionIndex < questions.length) {
+      print(this._questionIndex);
+    } else {
+      print("Última pergunta");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = {
-      'Se você tivesse oportunidade iria para qual desses países?',
-      'Quais idiomas você já fala?',
-      'Quais países você já visitou de férias?'
-    };
-
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              //title: Text('Love & Data & Science'),
-              title: Text(
-                  'Muitos brasileiros estão \n imigrando e isso gerou nossa \n curiosidade sobre as seguintes questões:'),
-            ),
-            body:
-                //Text('Love moves the world, but what we know about it?'),
-                Column(
-              children: [
-                Text(questions.elementAt(0)),
-                RaisedButton(
-                  child: Text('Estados Unidos'),
-                  onPressed: answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Canadá'),
-                  onPressed: () => {print('Canadá foi escolhido')},
-                ),
-                RaisedButton(
-                  child: Text('Australia'),
-                  onPressed: () { print("Australia foi escolhida");},
-                ),
-                RaisedButton(
-                  child: Text('Irlanda'),
-                  onPressed: answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Portugal'),
-                  onPressed: answerQuestion,
-                ),
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Experience abroad'),
+        ),
+        body: _questionIndex < questions.length
+            ?
+            //Text('Love moves the world, but what we know about it?'),
+            Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answerText) {
+                    return Answer(_answerQuestion, answerText);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text("Fim das perguntas"),
+              ),
+      ),
+    );
   }
 }
